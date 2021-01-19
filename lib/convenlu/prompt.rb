05d4e4a3b2c47ba713b44665de180a8dcc436076
julio cabrera <juliocabrera820@gmail.com>
2021-01-18 20:05:33 -0600

    refactor: call read_json method that returns the convention

--- lib/convenlu/prompt.rb
+++ lib/convenlu/prompt.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 require 'tty-prompt'
+require 'convenlu/reader'
 
 module Convenlu
   class Prompt
     def self.start
-      commit_types = %w[build chore ci docs feat fix perf refactor style test]
-      @prompt = TTY::Prompt.new
-      @prompt.collect do
-        response_scope = @prompt.yes?('do you want to add a scope?', default: false)
+      prompt = TTY::Prompt.new
+      prompt.collect do
+        response_scope = prompt.yes?('do you want to add a scope?', default: false)
         key(:scope).ask('enter the commit scope') if response_scope
-        key(:type).select('select the commit type', commit_types)
+        key(:type).select('select the commit type', Reader.read_json)
         key(:title).ask('write a short title', required: true)
         key(:description).ask('provide a longer description', required: true)
         response_footer = key(:footer).yes?('do you want to add a footer?', default: false)
