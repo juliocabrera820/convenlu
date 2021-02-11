e9e4f151896913bc0b2db5e9689d97b8606a4fb7
julio cabrera <juliocabrera820@gmail.com>
2021-02-11 10:24:44 -0600

    refactor: add OR operator

--- lib/convenlu/reader.rb
+++ lib/convenlu/reader.rb
@@ -7,9 +7,7 @@ module Convenlu
   class Reader
     def self.read_json
       relative_path = File.join(Dir.pwd, 'lib', 'convenlu', 'convention.json')
-      if Dir.pwd.split('/')[-1] == 'convenlu'
-        file = File.read(relative_path)
-      elsif File.exist?(relative_path)
+      if Dir.pwd.split('/')[-1] == 'convenlu' || File.exist?(relative_path)
         file = File.read(relative_path)
       else
         convention_absolute_path = File.join(Gem.dir, 'gems', "convenlu-#{Convenlu::VERSION}", 'lib', 'convenlu',
