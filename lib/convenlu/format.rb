4a375807ff33ed29bd43ce33a11c8432de10c045
juliocabrera820 <juliocabrera820@gmail.com>
2024-05-01 16:54:51 -0600

    style: apply rubocop suggestions

--- lib/convenlu/format.rb
+++ lib/convenlu/format.rb
@@ -1,13 +1,16 @@
 module Convenlu
   class Format
-    def self.to_plain_text(commit)
-      full_commit = ""
-      full_commit << commit[:type].split(':')[0]
-      full_commit << "(#{commit[:scope]})" if commit.key?(:scope)
-      full_commit << ": #{commit[:title]}"
-      full_commit << "\n\n#{commit[:description]}"
-      full_commit << "\n\n#{commit[:footer]}" if commit.key?(:footer)
-      full_commit
+    class << self
+      # Class to format commit message output
+      def to_plain_text(commit)
+        full_commit = ""
+        full_commit << commit[:type].split(":")[0]
+        full_commit << "(#{commit[:scope]})" if commit.key?(:scope)
+        full_commit << ": #{commit[:title]}"
+        full_commit << "\n\n#{commit[:description]}"
+        full_commit << "\n\n#{commit[:footer]}" if commit.key?(:footer)
+        full_commit
+      end
     end
   end
 end
